77efe8932a140770afd26da8aeb77ce0a00d6740
Hector G. Parra <hgparra+github@gmail.com>
2010-02-01 23:54:15 -0800

    Initial commit from conglomo code.

---
+++ VISCA.js
@@ -0,0 +1,92 @@
+var VISCA = {
+	CAM_Zoom: {
+		Stop: function(x) {
+			return "8" + x + " 01 04 07 00 FF";
+		},
+		TeleStandard: function(x,p) {
+			return "8" + x + " 01 04 07 2" + p + " FF";
+		},
+		WideStandard: function(x,p) {
+			return "8" + x + " 01 04 07 3" + p + " FF";
+		}
+	},
+	CAM_Backlight: {
+		On: function(x) {
+			return "8" + x + " 01 04 33 02 FF";
+		},
+		Off: function(x) {
+			return "8" + x + " 01 04 33 03 FF";
+		}
+	},
+	Pan_tiltDrive: {
+		Up: function(x, v, w) {
+			return "8" + x + " 01 06 01 " + v + " " + w +" 03 01 FF";
+		},
+		Down: function(x, v, w) {
+			return "8" + x + " 01 06 01 " + v + " " + w +" 03 02 FF";
+		},
+		Left: function(x, v, w) {
+			return "8" + x + " 01 06 01 " + v + " " + w +" 01 03 FF";
+		},
+		Right: function(x, v, w) {
+			return "8" + x + " 01 06 01 " + v + " " + w +" 02 03 FF";
+		},
+		Stop: function(x, v, w) {
+			return "8" + x + " 01 06 01 " + v + " " + w +" 03 03 FF";
+		},
+		Home: function(x) {
+			return "8" + x + " 01 06 04 FF";
+		}
+	}
+	
+	/*
+	command: function(camNum, subcommand) {
+		return "8" + camNum + " " + subcommand;
+	},
+	subcommand: {
+		// v : Pan Speed -> 01 to 18, 2 digit string
+		// w : Tilt Speed -> 01 to 17, 2 digit string
+		
+		panTiltDriveUp: function(v,w){
+			return "01 06 01 "+v+" "+w+" 03 01 FF";
+		},
+		
+		panTiltDriveDown: function(v,w){
+			return "01 06 01 "+v+" "+w+" 03 02 FF";
+		},
+		
+		panTiltDriveLeft: function(v,w){
+			return "01 06 01 "+v+" "+w+" 01 03 FF";
+		},
+		
+		panTiltDriveRight: function(v,w){
+			return "01 06 01 "+v+" "+w+" 02 03 FF";
+		},
+		
+		panTiltDriveStop: function(v,w){
+			return "01 06 01 "+v+" "+w+" 03 03 FF";
+		},
+		
+		panTiltDriveHome: function(){
+			return "01 06 04 FF";
+		},
+		
+		panTiltDriveStop: function(v, w) {
+			return "01 06 01 " + v + " " + w + " 03 03 FF";
+		},
+		
+		camZoomStop: function() {
+			return "01 04 07 00 FF";
+		},
+		
+		camZoomTeleStandard: function() {
+			return "01 04 07 02 FF";
+		},
+		
+		camZoomWideStandard: function() {
+			return "01 04 07 03 FF";
+		}
+	}
+	*/
+}
+
